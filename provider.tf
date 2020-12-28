1a3afbf7ba216bdb879ee7ef389f2b7e70636155
root <root@ip-172-31-41-243.ap-south-1.compute.internal>
2020-12-28 16:35:25 +0000

    Provider

---
+++ provider.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+        access_key = "AKIA2KF6FF5XHIIKWNXI"
+        secret_key = "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"
+        region = var.aws_region
+#       version = "~>2.0"
+}
