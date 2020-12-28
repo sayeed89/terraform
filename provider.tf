e36c5942fc024c7939911af889e7b52ef8e2c496
sayeed89 <37158975+sayeed89@users.noreply.github.com>
2020-12-28 22:13:06 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-        access_key = "AKIA2KF6FF5XHIIKWNXI"
-        secret_key = "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"
+        access_key = var.aws_access_key
+        secret_key = var.aws_secret_key
         region = var.aws_region
 #       version = "~>2.0"
 }
